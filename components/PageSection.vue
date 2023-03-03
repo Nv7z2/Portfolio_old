8581e12165fda4c25dbf1e3cc9c45d445ebdb80a
nv7z2 <barankacper2000@gmail.com>
2023-03-03 17:11:45 +0100

    feat: add page section component with divider

---
+++ components/PageSection.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    class="section-divider"
+    v-if="divider == 'top' || divider == 'both'"
+  ></div>
+
+  <section class="page-section">
+    <slot></slot>
+  </section>
+
+  <div
+    class="section-divider"
+    v-if="divider == 'bottom' || divider == 'both'"
+  ></div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  divider?: 'top' | 'bottom' | 'both' | 'none';
+}>();
+
+const divider = props.divider ?? 'none';
+</script>
+
+<style scoped lang="scss">
+@use '@/styles/colors';
+
+.section-divider {
+  height: 1px;
+  background-color: colors.$page-section-divider-color;
+}
+
+.page-section {
+  margin: 2rem 0;
+}
+</style>
