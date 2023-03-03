18bbcfc8277a833517347c587b5b5b779e1595ba
nv7z2 <barankacper2000@gmail.com>
2023-03-03 17:50:41 +0100

    refactor(section heading): add props instead of hard data

--- components/SectionHeading.vue
+++ components/SectionHeading.vue
@@ -1,11 +1,25 @@
 <template>
-  <h2 class="section-heading">
-    <span class="section-heading-prefix">&gt;</span>
+  <h2 class="section-heading" :style="headingStyles">
+    <span v-if="prefix || prefix != 'none'" class="section-heading-prefix">
+      {{ prefix }}
+    </span>
     <slot></slot>
   </h2>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const props = defineProps<{
+  /** Prefix to the heading. Default: ">". */
+  prefix?: string | 'none';
+  /** Margin in rems. For example :margin="1" equals 1rem. */
+  margin?: number;
+}>();
+
+const prefix = props.prefix ?? '>';
+const headingStyles = reactive({
+  marginBottom: props.margin ? `${props.margin}rem` : '0',
+});
+</script>
 
 <style scoped lang="scss">
 @use '@/styles/colors';
