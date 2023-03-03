e86dea52475ad517bc9357f42124e5bbb467f640
nv7z2 <barankacper2000@gmail.com>
2023-03-03 16:50:14 +0100

    feat: add mvp social anchor component

---
+++ components/SocialAnchor.vue
@@ -0,0 +1,44 @@
+<template>
+  <a :href="href" class="social-anchor" target="_blank" :title="href" rel="noopener noreferrer">
+    <img
+      :src="'icons/' + icon  + '.svg'"
+      class="social-anchor-icon"
+      role="presentation"
+    />
+  </a>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  href: string;
+  icon: "github" | "linkedin" | "twitter";
+}>();
+</script>
+
+<style scoped lang="scss">
+@use '@/styles/colors';
+
+.social-anchor {
+  display: inline-flex;
+  text-decoration: none;
+  padding: 0.5rem 1.5rem;
+  align-items: center;
+  border: 1px solid colors.$social-anchor-border;
+  border-radius: 0.5rem;
+  transition: all 0.2s ease;
+
+  &:hover {
+    box-shadow: 0 0.5rem 1rem -0.5rem colors.$social-anchor-hover-shadow;
+    transform: translateY(-0.125rem);
+  }
+
+  &:not(:last-child) {
+    margin-right: 1rem;
+  }
+}
+
+.social-anchor-icon {
+  width: 1.5rem;
+  height: 1.5rem;
+}
+</style>
