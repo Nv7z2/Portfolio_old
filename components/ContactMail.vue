ae9f183111df2c6550ccfc578f74ed548ac08597
nv7z2 <barankacper2000@gmail.com>
2023-03-03 18:52:09 +0100

    feat: add contact mail component

---
+++ components/ContactMail.vue
@@ -0,0 +1,23 @@
+<template>
+  <a :href="'mailto:' + mail" class="contact-mail">{{ mail }}</a>
+</template>
+
+<script setup lang="ts">
+type ContactMailProps = {
+  mail: string;
+};
+
+defineProps<ContactMailProps>();
+</script>
+
+<style scoped lang="scss">
+@use '@/styles/colors';
+
+.contact-mail {
+  color: colors.$contact-mail-color;
+
+  &:hover {
+    color: colors.$contact-mail-hover-color;
+  }
+}
+</style>
