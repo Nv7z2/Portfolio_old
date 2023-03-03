9e9561e79fdb882a17c1767d91659be6d190ee43
nv7z2 <barankacper2000@gmail.com>
2023-03-03 19:26:44 +0100

    feat: add text with border component

---
+++ components/TextWithBorder.vue
@@ -0,0 +1,26 @@
+<template>
+  <span class="text-with-color" :style="textWithColorStyles">
+    <slot></slot>
+  </span>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  color?: string;
+}>();
+
+const textWithColorStyles = reactive({
+  borderColor: props.color || 'inherit',
+});
+</script>
+
+<style scoped lang="scss">
+.text-with-color {
+  border-bottom: 2px solid currentColor;
+  transition: background-color 0.2s ease;
+
+  &:hover {
+    background-color: rgba(255, 255, 255, 0.075);
+  }
+}
+</style>
