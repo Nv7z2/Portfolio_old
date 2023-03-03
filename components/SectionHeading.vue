c9ac165641e6b200ea2a6d70d5eb77d9a6855927
nv7z2 <barankacper2000@gmail.com>
2023-03-03 16:58:31 +0100

    feat: add section heading component

---
+++ components/SectionHeading.vue
@@ -0,0 +1,23 @@
+<template>
+  <h2 class="section-heading">
+    <span class="section-heading-prefix">&gt;</span>
+    <slot></slot>
+  </h2>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+@use '@/styles/colors';
+
+.section-heading {
+  margin: 0;
+  font-weight: normal;
+  color: colors.$section-heading-color;
+}
+
+.section-heading-prefix {
+  color: colors.$section-heading-prefix-color;
+  margin-right: 0.5rem;
+}
+</style>
