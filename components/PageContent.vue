525c95cf77c9827f916c68cdd2e19008f0711a05
nv7z2 <barankacper2000@gmail.com>
2023-03-03 16:22:25 +0100

    feat: add page content component & prepare layout

---
+++ components/PageContent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="page-content">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use '@/styles/colors';
+
+.page-content {
+  padding: 1rem;
+  margin-left: 2rem;
+  color: colors.$page-content-color;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,23 @@
 <template>
-  <div>
-    Siemano
-    <slot></slot>
+  <div class="layout layout-default">
+    <Navigation />
+    <PageContent>
+      <slot></slot>
+    </PageContent>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Navigation from '@/components/Navigation.vue';
+import PageContent from '@/components/PageContent.vue';
+</script>
 
-<style scoped></style>
+<style scoped lang="scss">
+@use '@/styles/variables' as sizes;
+
+.layout-default {
+  display: flex;
+  max-width: sizes.$site-max-width;
+  margin: 8rem auto;
+}
+</style>
