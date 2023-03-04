daf3c44ade354edbb29c19062c04f5c8386611d4
nv7z2 <barankacper2000@gmail.com>
2023-03-04 23:50:32 +0100

    feat: load experiences from API

--- pages/experience.vue
+++ pages/experience.vue
@@ -3,7 +3,7 @@
     <Section-Heading prefix="📜" :margin="1">Experience</Section-Heading>
     <Page-Section :divider="{ type: 'top' }">
       <ExperienceItem
-        v-for="experience in ExperienceList"
+        v-for="experience in experiences"
         :key="experience.company"
         :company="experience.company"
         :position="experience.position"
@@ -19,44 +19,14 @@
 import type { ExperienceItemType } from '@/components/Experience/ExperienceItem.vue';
 import ExperienceItem from '@/components/Experience/ExperienceItem.vue';
 
-// In future this list will be fetched from an API. I promise.
-// TODO: Fetch experience list from API.
-const ExperienceList: ExperienceItemType[] = [
-  {
-    company: 'Dephug (self-employed)',
-    position: 'Unicorn developer',
-    startDate: 'June 2021',
-    endDate: 'Nov 2022',
-  },
-  {
-    company: 'BracketSpace',
-    position: 'Junior front-end developer',
-    link: 'https://www.bracketspace.com/',
-    startDate: 'Apr 2022',
-    endDate: 'Aug 2022',
-  },
-  {
-    company: 'Balticon S. A.',
-    position: 'Junior front-end developer',
-    link: 'https://www.balticon.pl/',
-    startDate: 'Apr 2021',
-    endDate: 'Nov 2021',
-  },
-  {
-    company: 'Balticon S. A.',
-    position: 'Junior IT Support Specialist',
-    link: 'https://www.balticon.pl/',
-    startDate: 'Nov 2020',
-    endDate: 'Apr 2021',
-  },
-  {
-    company: 'ANSTA Smart Solutions',
-    position: 'Intern front-end developer',
-    link: 'https://www.ansta.pl/',
-    startDate: 'Oct 2019',
-    endDate: 'Jan 2020',
-  },
-];
+import { ref } from 'vue';
+
+let experiences = ref<ExperienceItemType[]>([]);
+const config = useRuntimeConfig();
+
+await useFetch(config.public.apiBase + '/experiences').then(
+  (res) => (experiences = res.data),
+);
 </script>
 
 <style scoped lang="scss"></style>
