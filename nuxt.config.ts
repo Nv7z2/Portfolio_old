f271a4125c6583cabd3e944a13266d85727a9472
nv7z2 <barankacper2000@gmail.com>
2023-03-04 16:25:55 +0100

    feat: add api to load data from server

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,10 +5,16 @@ const config = defineNuxtConfig({
   alias: {
     icons: path.resolve(__dirname, 'public/icons'),
   },
+  modules: [
+    '@nuxtjs/dotenv',
+  ]
 });
 
 export default {
   target: 'server',
   mode: 'static',
+  env: {
+    apiUrl: process.env.API_URL,
+  },
   config,
 };
