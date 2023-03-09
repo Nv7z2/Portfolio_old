3c9ee9fc930b7163f92b1338d809f7f4313cdcd2
nv7z2 <barankacper2000@gmail.com>
2023-03-09 18:48:22 +0100

    feat(pages): add translation page view
    But this view does not appear in the navigation

---
+++ pages/translations.vue
@@ -0,0 +1,32 @@
+<template>
+  <main class="page page-experience">
+    <Section-Heading prefix="🇵🇱" :margin="1">Translations</Section-Heading>
+    <Page-Section :divider="{ type: 'top' }">
+      <TranslationItem
+        v-for="translation in translations"
+        :key="translation.id"
+        :id="translation.id"
+        :url="translation.url"
+        :description="translation.description"
+        :title="translation.title"
+        :tags="translation.tags"
+      />
+    </Page-Section>
+  </main>
+</template>
+
+<script setup lang="ts">
+import type { TranslationItemType } from '@/components/Translations/TranslationItem.vue';
+import TranslationItem from '@/components/Translations/TranslationItem.vue';
+
+import { ref } from 'vue';
+
+let translations = ref<TranslationItemType[]>([]);
+const config = useRuntimeConfig();
+
+await useFetch(config.public.apiBase + '/translations').then(
+  (res) => (translations = res.data),
+);
+</script>
+
+<style scoped lang="scss"></style>
